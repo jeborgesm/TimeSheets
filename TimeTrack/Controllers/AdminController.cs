f955fa90b46278094cc4b237225ced163f250345
jeborges <jeborgesm@hotmail.com>
2018-06-07 23:27:58 -0400

    Changed configuration to use external db using connection string. Added dacpac file to restore db in external service. Added View model ato sendthe data to be edited. modified the style of the admin pag to use Bootstrap pill tabs.

--- TimeTrack/Controllers/AdminController.cs
+++ TimeTrack/Controllers/AdminController.cs
@@ -1,24 +1,26 @@
-﻿using System.Web.Mvc;
-using Time.Data;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using TimeTrack.Models;
 
 namespace TimeTrack.Controllers
 {
     public class AdminController : Controller
     {
-        private ITimeRepository _repository;
+        //private ITimeRepository _repository;
 
-        public AdminController() : this(new TimeRepository())
-        {
-        }
+        //public AdminController() : this(new TimeRepository())
+        //{
+        //}
 
-        public AdminController(ITimeRepository repository)
-        {
-            _repository = repository;
-        }
+        //public AdminController(ITimeRepository repository)
+        //{
+        //    _repository = repository;
+        //}
 
         public ActionResult Index()
         {
-            return View();
+            Admin model = TimeTrack.Models.Admin.GetAllItems();
+            return View(model);
         }
 
     }
---
+++ TimeTrack/Models/Admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Time.Data;
+
+namespace TimeTrack.Models
+{
+    public class Admin
+    {
+        public IEnumerable<Client> Clients { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Resource> Resources { get; set; }
+
+        public static Admin GetAllItems()
+        {
+            Time.Data.TimeRepository repo = new TimeRepository();
+            Admin adm = new Admin
+            {
+                Clients = repo.ListAllClients()
+            };
+            return adm;
+        }
+    }
+}
